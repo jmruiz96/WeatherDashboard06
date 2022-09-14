86dae796110c5cbbcf93e05cc3561265eb32a2d2
jmruiz96 <111460342+jmruiz96@users.noreply.github.com>
2022-09-13 22:01:59 -0500

    citys are grabbed from api, current weather is displayed, and forecast is displayed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,13 +14,21 @@ function getCurrentWeather (){
     .then(data => { 
         console.log(data)
         displayCurrentWeather(data)
-        displayForecast(data, 0)
-        displayForecast(data, 1)
-        displayForecast(data, 2)
-        displayForecast(data, 3)
-        displayForecast(data, 4)
     })
-}
+
+    urlApiFc = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&APPID=7a480e36f75524b0ccdac9a4e3614bcf&units=imperial&APPID";
+    fetch(urlApiFc)
+    .then(res => res.json())
+    .then(data1 => { 
+        console.log(data1)
+        displayForecast(data1, 0)
+        displayForecast(data1, 1)
+        displayForecast(data1, 2)
+        displayForecast(data1, 3)
+        displayForecast(data1, 4)
+})
+};
+
 
 
 //Current weather conditions for that city// 
@@ -45,23 +53,22 @@ function displayCurrentWeather (data) {
 }
 
 //5-day forcast
-function displayForecast (data,index) {
-    // for (var i = 0; i < (data.list.length); i++) {
-    //     if (i===4 || i===12 || i===20 || i===28 || i===36) {
+function displayForecast (data1,index) {
+    
     var dayH2 = $("#date" + (index + 1));
     dayH2.text(dayjs().add(index + 1, "day").format("MMMM DD, YYYY"));
 
     var tempH4 = $("#temp" + (index + 1));
-    tempH4.text(`Temp: ${data.main.temp}\u00B0F`);
+    tempH4.text(`Temp: ${data1.list[index].main.temp}\u00B0F`);
 
     var humidityH4 = $("#hum" + (index + 1));
-    humidityH4.text(`Wind: ${data.wind.speed} MPH`);
+    humidityH4.text(`Wind: ${data1.list[index].wind.speed} MPH`);
 
     var windSpeedH4 = $("#wind" + (index + 1));
-    windSpeedH4.text(`Humidity: ${data.main.humidity}%`);
+    windSpeedH4.text(`Humidity: ${data1.list[index].main.humidity}%`);
 
     var icon = $("#icon" + (index + 1))
-    icon.src = "http://openweathermap.org/img/wn/"+ data.daily[index].weather[0].icon +"@2x.png"
+    icon.src = "http://openweathermap.org/img/wn/"+ data1.list[index].weather[0].icon +"@2x.png"
 }
 // }
 // }
